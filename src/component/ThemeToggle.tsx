f95c09a223bfec6ca9783761d2243afb388e9150
Mohamad Osman Goni <osmangoni0827@gmail.com>
2024-10-26 12:08:07 +0600

    update

--- src/component/ThemeToggle.tsx
+++ src/component/ThemeToggle.tsx
@@ -1,25 +1,25 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function DarkModeToggle() {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+  const [theme, setTheme] = useState<"light" | "dark">("light");
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const previousTheme = localStorage.getItem('theme') || 'light';
-      const rootClass= document.documentElement.classList
-     rootClass.add(previousTheme);
-      setTheme(previousTheme as 'light' | 'dark');
+    if (typeof window !== "undefined") {
+      const previousTheme = localStorage.getItem("theme") || "light";
+      const rootClass = document.documentElement.classList;
+      rootClass.add(previousTheme);
+      setTheme(previousTheme as "light" | "dark");
     }
   }, []);
 
   const toggleTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    const rootClass= document.documentElement.classList
-   rootClass.remove(theme);
+    const newTheme = theme === "light" ? "dark" : "light";
+    const rootClass = document.documentElement.classList;
+    rootClass.remove(theme);
     rootClass.add(newTheme);
-    localStorage.setItem('theme', newTheme);
+    localStorage.setItem("theme", newTheme);
     document.cookie = `theme=${newTheme}; path=/; max-age=31536000`;
     setTheme(newTheme);
   };
@@ -27,9 +27,9 @@ export default function DarkModeToggle() {
   return (
     <button
       onClick={toggleTheme}
-      className="text-md font-semibold px-4 py-2 border-2  border-gray-600 rounded-md bg-slate-400   dark:bg-gray-800 text-gray-600 dark:text-white"
+      className="text-md font-semibold px-4 py-2 border-2  border-gray-600 rounded-md bg-gray-800  dark:bg-gray-600 text-gray-600 dark:text-white"
     >
-      {theme === 'light' ? 'Dark' : 'Light'} Mode
+      {theme === "light" ? "Dark" : "Light"} Mode
     </button>
   );
 }
